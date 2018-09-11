f265ded48dd833b22bf3ba4e92941be21c57e138
jdj <1561624238@qq.com>
2018-09-11 16:56:34 +0800

    修改图片路径

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class App extends Component{
     render(){
         return(
             <div className='app-container'>
-                <img src="../static/img/logo" />
+                <img src={require("../static/img/logo")} />
                 <p className="logo-text">jdj react</p>
                 <p className="slogon-text">不念过去、不畏将来</p>
             </div>
