9f070f627e7640836d38d4657157b4d4e440610a
djjiang <1561624238@qq.com>
2019-01-30 11:08:21 +0800

    修改react脚手架

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import "../static/component/app.scss";
 import React, { Component } from 'react';
 
 class App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            logo: require('#/img/logo.jpg')
+        }
+    }
     render(){
         return(
             <div className='app-container'>
-                <img src={require("../static/img/logo")} />
+                <img src={this.state.logo} />
                 <p className="logo-text">jdj react</p>
                 <p className="slogon-text">不念过去、不畏将来</p>
             </div>
